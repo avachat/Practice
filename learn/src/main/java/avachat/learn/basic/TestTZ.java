cfd1f3c1cf925d28ddc8e39dd150703060164a9a
avachat <avachat@hotmail.com>
2015-12-13 12:24:46 -0800

    Checking git integration

--- learn/src/main/java/avachat/learn/basic/TestTZ.java
+++ learn/src/main/java/avachat/learn/basic/TestTZ.java
@@ -6,6 +6,7 @@ import java.util.TimeZone;
 public class TestTZ {
 
     public static void main(String args[]) {
+        // Checking if git integration works
         System.out.println("Current time: " + (new Date()).toString());
         System.out.println("Current time zone: " + TimeZone.getDefault().getID());
     }
