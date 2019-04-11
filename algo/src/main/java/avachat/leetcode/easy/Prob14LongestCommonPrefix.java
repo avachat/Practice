2c8b73849870db6b18921ca9001a69925ed83834
Abhay Avachat <abhava01@C02WG16DHTDD.sjc.arm.com>
2019-04-11 13:32:51 -0700

    Problem 14

---
+++ algo/src/main/java/avachat/leetcode/easy/Prob14LongestCommonPrefix.java
@@ -0,0 +1,72 @@
+package avachat.leetcode.easy;
+
+
+/*
+
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+Example 1:
+
+Input: ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+Note:
+
+All given inputs are in lowercase letters a-z.
+
+ */
+
+public class Prob14LongestCommonPrefix {
+
+
+    /*
+    NOTE : The extra stringbuilder wasn't necessary. A substring would be better.
+     */
+
+    public String longestCommonPrefix(String[] allStrings) {
+
+        if ( allStrings.length == 0 ) {
+            return "";
+        }
+
+        String baseStr = allStrings[0];
+        if ( (null == baseStr) || baseStr.isEmpty()) {
+            return "";
+        }
+
+        StringBuilder prefix = new StringBuilder("");
+
+        for ( int i = 0; i < baseStr.length(); i++) {
+            char c = baseStr.charAt(i);
+            if ( ! matchCharAtPosition(allStrings, baseStr.charAt(i), i)) {
+                break;
+            }
+            prefix.append(c);
+        }
+
+        return prefix.toString();
+    }
+
+    private boolean matchCharAtPosition (String[] allStrings, char c, int position) {
+
+        for (int s = 1; s < allStrings.length ; s++) {
+            if (allStrings[s] == null) {
+                return false;
+            }
+            if (allStrings[s].length() <= position) {
+                return false;
+            }
+            if ( c != allStrings[s].charAt(position)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ algo/src/test/java/avachat/leetcode/easy/Prob14LongestCommonPrefixTest.java
@@ -0,0 +1,37 @@
+package avachat.leetcode.easy;
+
+import org.junit.Test;
+
+import static org.testng.Assert.*;
+
+public class Prob14LongestCommonPrefixTest {
+
+    private Prob14LongestCommonPrefix testObj = new Prob14LongestCommonPrefix();
+
+    @Test
+    public void testHappyPath() {
+        assertEquals(testObj.longestCommonPrefix(new String[]{"abcd", "abcxxxxx", "abcyyyyyyyy"}), "abc");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"abz", "abcxxxxx", "abcyyyyyyyy", "abab", "abbbb", "abaaa"}), "ab");
+    }
+
+    @Test
+    public void testNothingCommon() {
+        assertEquals(testObj.longestCommonPrefix(new String[]{"aaa", "bbb"}), "");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"baa", "abb"}), "");
+    }
+
+    @Test
+    public void testEdgeConditions() {
+        assertEquals(testObj.longestCommonPrefix(new String[]{}), "");
+        assertEquals(testObj.longestCommonPrefix(new String[]{null}), "");
+        assertEquals(testObj.longestCommonPrefix(new String[]{""}), "");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"a"}), "a");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"ab"}), "ab");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"ab", ""}), "");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"ab", null}), "");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"ab", null, ""}), "");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"ab", "ab", ""}), "");
+        assertEquals(testObj.longestCommonPrefix(new String[]{"ab", "ab", "ab"}), "ab");
+    }
+
+}
