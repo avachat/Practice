8fd9d16960d31dd98f8604ebff367ff627be46f5
Abhay Avachat <abhava01@c02wg16dhtdd.sjc.arm.com>
2019-04-24 14:53:28 -0700

    Finished problem 35

---
+++ algo/src/main/java/avachat/leetcode/easy/Prob35SearchInsertPosition.java
@@ -0,0 +1,73 @@
+package avachat.leetcode.easy;
+
+public class Prob35SearchInsertPosition {
+
+    /*
+    Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+Example 3:
+
+Input: [1,3,5,6], 7
+Output: 4
+Example 4:
+
+Input: [1,3,5,6], 0
+Output: 0
+     */
+
+
+    public int searchInsert(int[] nums, int target) {
+
+        if ( (null == nums) || (0 == nums.length)) {
+            throw new IllegalArgumentException("nums is empty:)");
+        }
+
+        return searchInsertRecursive(nums, 0, nums.length-1, target);
+    }
+
+    private int searchInsertRecursive(int[] nums, int startIndex, int endIndex, int target) {
+
+        /*
+        # stupid errors : see below
+         */
+
+        if (startIndex == endIndex) {
+            if (nums[startIndex] == target) {
+                return startIndex;
+            }
+            if ( target > nums[startIndex] ) { // MISSED : initial comparison was target > startIndex :-(
+                return startIndex+1;
+            } else {
+                return startIndex;
+                /*
+                if (startIndex == 0) {
+                    return 0;
+                } else {
+                    return startIndex; // MISSED : initiallyw as returning startIndex - 1
+                }
+                */
+            }
+        }
+
+        int middle = (startIndex + endIndex)/2; // MISSED : Divide by 2
+        if ( target == nums[middle]) {
+            return middle;
+        } else if ( target < nums[middle]) {
+            return searchInsertRecursive(nums, startIndex, middle, target);
+        } else {
+            return searchInsertRecursive(nums, middle+1, endIndex, target);
+        }
+    }
+
+
+}
