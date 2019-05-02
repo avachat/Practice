c2ee62a5eed1c386aaae793e32735020b5f4faa8
Abhay Avachat <abhava01@C02WG16DHTDD.sjc.arm.com>
2019-05-02 15:40:55 -0700

    Prob 100 same tree

---
+++ algo/src/main/java/avachat/leetcode/easy/Prob100SameTree.java
@@ -0,0 +1,68 @@
+package avachat.leetcode.easy;
+
+public class Prob100SameTree {
+
+
+    /*
+    Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+     */
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+
+        /*
+
+        GOOD : Got concise solution in 2 mins
+
+         */
+
+        if ( (null == p) || (null == q) ) {
+            return (p==q);
+        }
+
+        return ( (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+    }
+
+}
