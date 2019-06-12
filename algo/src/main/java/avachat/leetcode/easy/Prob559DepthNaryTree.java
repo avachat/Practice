5c21838cc1c61ec401830884a3f008b4cd780fab
Abhay Avachat <abhava01@c02wg16dhtdd.sjc.arm.com>
2019-06-11 17:33:14 -0700

    Solving 559, 561

---
+++ algo/src/main/java/avachat/leetcode/easy/Prob559DepthNaryTree.java
@@ -0,0 +1,69 @@
+package avachat.leetcode.easy;
+
+import java.util.List;
+
+public class Prob559DepthNaryTree {
+
+    /*
+
+    Given a n-ary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+For example, given a 3-ary tree:
+
+
+
+
+
+
+We should return its max depth, which is 3.
+
+
+
+Note:
+
+The depth of the tree is at most 1000.
+The total number of nodes is at most 5000.
+
+     */
+
+    private static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val,List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+    public int maxDepth(Node root) {
+
+        /*
+
+        GOOD : Quick impl, fast execution time > 99%
+
+        BAD : Forgot add 1 :-( :-(
+         */
+
+        if (null == root) {
+            return 0;
+        }
+
+        int maxDepth = 0;
+
+        for (Node child : root.children) {
+            int depth = maxDepth(child);
+            if (depth > maxDepth) {
+                maxDepth = depth;
+            }
+        }
+
+        return maxDepth + 1; // FORGOT to add 1
+
+    }
+
+}
---
+++ algo/src/main/java/avachat/leetcode/easy/Prob561ArrayPartition.java
@@ -0,0 +1,42 @@
+package avachat.leetcode.easy;
+
+import java.util.Arrays;
+
+public class Prob561ArrayPartition {
+
+    /*
+
+    Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+Example 1:
+Input: [1,4,3,2]
+
+Output: 4
+Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+Note:
+n is a positive integer, which is in the range of [1, 10000].
+All the integers in the array will be in the range of [-10000, 10000].
+
+     */
+
+
+    public int arrayPairSum(int[] nums) {
+
+        /*
+
+        GOOD : Got solution in 2 mins, with a proof c0mpleted in mind
+               Very quick impl, got correct the first time
+
+        BAD : Why faster than only 92%?
+         */
+
+        Arrays.sort(nums);
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i +=2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+}
