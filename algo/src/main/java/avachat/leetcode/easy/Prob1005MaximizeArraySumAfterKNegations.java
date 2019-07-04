5b7fc1f250d1bf204995543458306cfcca945054
avachat <avachat@hotmail.com>
2019-07-04 11:57:29 -0700

    Update Prob1005MaximizeArraySumAfterKNegations.java

--- algo/src/main/java/avachat/leetcode/easy/Prob1005MaximizeArraySumAfterKNegations.java
+++ algo/src/main/java/avachat/leetcode/easy/Prob1005MaximizeArraySumAfterKNegations.java
@@ -6,7 +6,9 @@ public class Prob1005MaximizeArraySumAfterKNegations {
 
     /*
 
-    iven an array A of integers, we must modify the array in the following way: we choose an i and replace A[i] with -A[i], and we repeat this process K times in total.  (We may choose the same index i multiple times.)
+    iven an array A of integers, we must modify the array in the following way:
+    we choose an i and replace A[i] with -A[i], and we repeat this process K times in total. 
+    (We may choose the same index i multiple times.)
 
 Return the largest possible sum of the array after modifying it in this way.
 
