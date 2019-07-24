384947ca31504e29a0f199003ddc3cc0bdca4ec7
Abhay Avachat <abhava01@C02WG16DHTDD.sjc.arm.com>
2019-07-24 16:12:36 -0700

    Adding user table

--- interview/Chegg/src/main/java/avachat/persistence/entity/User.java
+++ interview/Chegg/src/main/java/avachat/persistence/entity/User.java
@@ -12,6 +12,17 @@ import javax.persistence.*;
 @ToString
 public class User {
 
+
+    /*
+    Table created using :
+    CREATE TABLE chegg.user (
+        user_id BIGINT NOT NULL PRIMARY KEY,
+        login_handle VARCHAR(20) NOT NULL,
+        is_active BOOLEAN NOT NULL
+    );
+
+     */
+
     // TODO : Not sure why a sequence name has to be specified here, and not in other example in package learn.
     // TODO : Could be because of the table name being 'user', and that conflicts somehow with postgres user?
     // TODO : Without the explicit sequence name, get a runtime error saying hibernate_sequence does not exist
